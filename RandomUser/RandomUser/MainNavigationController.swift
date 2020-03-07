553dcd4000eccd3d7da0681ede42edba9a66e534
Bogdan Arsenkin <zigmuntb@gmail.com>
2020-03-07 14:51:01 +0200

    Removed storyboard and created xib. Created MainViewController.

---
+++ RandomUser/RandomUser/MainNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  MainNavigationController.swift
+//  RandomUser
+//
+//  Created by Arsenkin Bogdan on 07.03.2020.
+//  Copyright © 2020 Arsenkin Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    
+}
